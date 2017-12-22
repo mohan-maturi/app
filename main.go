04e417680e48c8a8caddcdeb7cde5606fecf1e91
Mohan Maturi <mohan.maturi@nutanix.com>
2017-12-21 18:01:05 -0800

    added MaturiWorld as dependency

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import "fmt"
+import "github.com/mohan-maturi/hutil/h"
+import mh "github.com/MaturiWorld/hutil/h"
 
 func main() {
     fmt.Println("hello world")
+    fmt.Println("\n\nAccessing mohan maturi")
+    fmt.Printf("%s\n", h.PrintHello("Anika"))
+    fmt.Println("\n\nAccessing maturi world")
+    fmt.Printf("%s\n", mh.PrintHello("Trisha"))
 }
